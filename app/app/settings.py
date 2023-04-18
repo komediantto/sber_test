4687bc46f6247a44346ea24cded8c2e3482d5daa
komediantto <komediantto@yandex.ru>
2023-04-18 11:01:13 +0200

    remove .env

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,11 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
 from pathlib import Path
-from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-vb!mdfwbsy4m#wq2)3d6$b0bc93ckroh5q44*viny$&j_p0l$$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
